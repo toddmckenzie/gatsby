a1b50add4ac73288fee04bc981c4c899cbb037ea
Todd McKenzie <toddmckenzie@hotmail.com>
2020-03-13 21:17:32 -0500

    removed commment from screen

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Navbar from './Navbar.js';
 import Footer from './Footer.js';
 
+//Need this to display info in the middle of navbar and footer.
 
 const Layout = ({ children }) => {
     return (
         <>
             <Navbar />
-            {children} //Need this to display info in the middle of navbar and footer.
+            {children} 
             <Footer />
         </>
     )
