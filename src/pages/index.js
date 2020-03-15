6f2e501f3691954ddd9360c20becee6ce1dd45ae
Todd McKenzie <toddmckenzie@hotmail.com>
2020-03-15 14:02:34 -0500

    made image folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,11 @@ import React from "react"
 import Tours from './tours.js';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
-import 'bootstrap/dist/css/bootstrap.min.css'
-// import "./layout.css";
 
 export default () => 
 <div>
     <Layout>
         <h1>Hello!</h1>
-        <div className="container">
-            <div className="row">
-                <div className="col-4">Hello</div>
-                <div className="col-4">Hello</div>
-                <div className="col-4">Hello</div>
-            </div>
-        </div>
         <Link to="/tours">Tours</Link>
     </Layout>
     
