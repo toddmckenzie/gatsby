9d558da18cd84a4660ec9a20f6b0b45dc139a7c9
Todd McKenzie <toddmckenzie@hotmail.com>
2020-03-15 15:51:34 -0500

    mapped through links in nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from '../css/navbar.module.css';
+import { FaAlignRight } from 'react-icons/fa';
+import links from '../constants/links';
+import socialIcons from '../constants/social-icons';
+import logo from '../images/logo.svg';
+import { Link } from 'gatsby'
 
 
 const Navbar = () => {
+    const [ isOpen, setNav ] = useState(false);
+
+    const toggleNav = () => {
+        setNav(!isOpen)
+    }
+    console.log(isOpen)
+
     return (
-        <div>
-            <h1>I am a navbar</h1>
-        </div>
+        <nav className={styles.navbar}>
+            <div className={styles.navCenter}>
+                <div className={styles.navHeader}>
+                    <img src={logo} alt="backroads logo" />
+                    <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+                        <FaAlignRight className={styles.logoIcon} />
+                    </button>
+                </div>
+                {links.map((item, index) => {
+                    return (<li key={index}><Link to={item.path}>{item.text}</Link></li>)
+                })}
+            </div>
+        </nav>
     )
 }
 
