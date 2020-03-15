eee50929ec720760a0dbaedd69d8eebd76f32b78
Todd McKenzie <toddmckenzie@hotmail.com>
2020-03-15 16:17:54 -0500

    navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,18 +17,44 @@ const Navbar = () => {
 
     return (
         <nav className={styles.navbar}>
-            <div className={styles.navCenter}>
-                <div className={styles.navHeader}>
-                    <img src={logo} alt="backroads logo" />
-                    <button type="button" className={styles.logoBtn} onClick={toggleNav}>
-                        <FaAlignRight className={styles.logoIcon} />
-                    </button>
-                </div>
-                {links.map((item, index) => {
-                    return (<li key={index}><Link to={item.path}>{item.text}</Link></li>)
-                })}
-            </div>
-        </nav>
+        <div className={styles.navCenter}>
+          <div className={styles.navHeader}>
+            <img src={logo} alt="backroads logo" />
+            <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+              <FaAlignRight className={styles.logoIcon} />
+            </button>
+          </div>
+          <ul
+            className={
+              isOpen
+                ? `${styles.navLinks} ${styles.showNav}`
+                : `${styles.navLinks}`
+            }
+          >
+            {links.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link to={item.path}>{item.text}</Link>
+                </li>
+              )
+            })}
+          </ul>
+          <div className={styles.navSocialLinks}>
+            {socialIcons.map((item, index) => {
+              return (
+                <a
+                  key={index}
+                  href={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {item.icon}
+                </a>
+              )
+            })}
+          </div>
+        </div>
+      </nav>
     )
 }
 
