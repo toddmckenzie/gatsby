22f848bb85f2e3860ab4cfabf436238eb8907ac0
Todd McKenzie <toddmckenzie@hotmail.com>
2020-03-16 19:42:41 -0500

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import styles from '../css/footer.module.css';
+import links from '../constants/links';
+import socialIcons from '../constants/social-icons';
+import { Link } from 'gatsby';
+import { FaRProject } from 'react-icons/fa';
 
 
 const Footer = () => {
     return (
-        <div>
-            <h1>I am a footer</h1>
-        </div>
+       <footer className={styles.footer}>
+           <div className={styles.links}>
+               {links.map((item, index) => {
+                   return <Link key={index} to={item.mpat}>{item.text}</Link>
+               })}
+           </div>
+           <div className={styles.icons}>
+               {socialIcons.map((item, index) => {
+                   return <a key={index} href={item.url} target="_blank" rel="noopener noreferr">
+                       {item.icon}
+                   </a>
+               })}
+           </div>
+       </footer>
     )
 }
 
