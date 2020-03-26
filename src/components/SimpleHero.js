041a4f626a88f7c6e814b782f1a30885d38d8fba
Todd McKenzie <toddmckenzie@hotmail.com>
2020-03-25 21:08:36 -0500

    working on simpleHero comp

---
+++ src/components/SimpleHero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//work on banner comp next
+
+const SimpleHero = ({children}) => {
+    return (
+      <div>
+          SimpleHero
+      </div>
+    )
+}
+
+
+export default SimpleHero;
