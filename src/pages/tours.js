1dab17d335c25b32edc2a607615e9c9dd0fa51d8
Todd McKenzie <toddmckenzie@hotmail.com>
2020-05-01 21:24:07 -0500

    changes in styling

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../components/Layout.js';
 import Header from '../examples/Header';
+import Images from '../components/Images';
 
 const Tours = () => {
     return (
         <Layout>
             <Header />
+            <Images />
         </Layout>
     )
 }
