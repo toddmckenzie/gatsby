c612d8d54a560ec57adbc0e29b72e235fc369f1d
Todd McKenzie <toddmckenzie@hotmail.com>
2020-05-13 17:06:58 -0500

    any changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
+
 const getAbout = graphql`
     query aboutImage{
         aboutImage:file(relativePath:{eq:"defaultBcg.jpeg"}){
--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaFacebook, FaTwitterSquare, FaSquarespace } from 'react-icons/fa';
 
-
+//
 export default [
     {
         icon: <FaFacebook />,
